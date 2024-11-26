4dacd4fc1d947d80eb6eb434e260e044753c417c
Nguyễn Minh Khang <110124083@st.tvu.edu.vn>
2024-11-26 08:31:50 +0700

    bai20buoi5.cpp

--- bai20buoi5.cpp
+++ bai20buoi5.cpp
@@ -80,7 +80,7 @@ void BurbleSort(float a[], int n){
 	for (int i=0;i<n-1;i++){
 		for (int j=n-1;j>i;j--){
 			if (a[j]<a[i]){
-				int tmp=a[i];
+				float tmp=a[i];
 				a[i]=a[j];
 				a[j]=tmp;
 			}
